e667e1523e1fc670b2d07707a55166faafb0f1a6
Routin hugo <routin.hugo74@orange.fr>
2025-02-13 20:52:21 +0100

    Update Carte eclairement.py

--- Carte eclairement.py
+++ Carte eclairement.py
@@ -5,3 +5,38 @@ Created on Thu Feb 13 20:35:27 2025
 @author: routin
 """
 
+import numpy as np
+
+class Source:
+    def __init__(self, x, y, z, I0, delta, theta, zeta, name='René'):
+        """
+        x, y, z : floats 
+                coordonnées de position de la source
+        I0 : float
+                Intensité de la source (de base)
+        delta: float
+                 je sais pas encore
+        theta: float
+                je sais pas encore
+        zeta: float
+                je sais pas encore   
+        """
+        self.x=x
+        self.y=y
+        self.z=z
+        self.I0=I0
+        self.delta=delta
+        self.theta=theta
+        self.zeta=zeta
+        self.name=name
+        
+    def intensity(self, angle, delta, I0):
+        return I0 * np.exp(- (4 * np.log(2)) * (angle / Delta) ** 2)
+    
+    def illumination(self, angle, distance):
+        
+        
+class Surface:
+    def __init__(self, )
+        
+        
